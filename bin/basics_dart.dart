55177c96ac19bb4e9ebf6dffaf37ff07e1d216c0
DIVYAKV15 <dhivya.kv1992@gmail.com>
2023-09-18 10:20:25 +0300

    first commit

---
+++ bin/basics_dart.dart
@@ -0,0 +1,15 @@
+
+
+void main() 
+{
+String name= "dhivya";
+int age=30;
+int phone=098765432;
+double cgpa=8.1;
+String email_id='dhivya.kv1992@gmail.com';
+print('MY NAME IS $name ');
+print('$age  IS MY AGE');
+print('MY PHONE NUMBER IS $phone');
+print('$cgpa IS MY CGPA SCORE IN GRADUATION');
+print('MY EMAIL_ID IS $email_id');
+}
---
+++ bin/getting_input.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+
+void main()
+{
+  print('hello world');
+  var a=stdin.readLineSync();
+  print('hello $a');
+
+}
